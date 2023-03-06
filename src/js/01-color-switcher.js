f9cb1106d9db9cd4e6a527ca4164f9b3fcabc409
Konstantin Martynenko <konstantin.vents@gmail.com>
2023-03-06 15:37:48 +0200

    Update 01-color-switcher.js

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,6 +1,7 @@
 const body = document.querySelector('body');
 const startBtn = document.querySelector('button[data-start]');
 const stopBtn = document.querySelector('button[data-stop]');
+let timerId = null;
 let intervalId = null;
  
 
@@ -13,16 +14,21 @@ startBtn.addEventListener('click', onStartBtnClick);
 stopBtn.disabled = true;
 
 function onStartBtnClick() {
-     intervalId = setInterval(() => {        
+    const timerId = setTimeout(() => {
+      
+     intervalId = setInterval(() => {
         startBtn.disabled = true;
         stopBtn.disabled = false;
         body.style.backgroundColor = getRandomHexColor();
-    }, 1000)
+     }, 1000)
+        
+    }, 0);
 };
 
 stopBtn.addEventListener('click', onStopBtnClick);
 
 function onStopBtnClick() { 
+    clearTimeout(timerId);
     clearInterval(intervalId);
     stopBtn.disabled = true;
     startBtn.disabled = false;
